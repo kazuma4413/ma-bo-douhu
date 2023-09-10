3e242290914a6a9887d998380a6e3b4c06fb9197
Noguchi-Kanta <a21ja115@st.osaka-cu.ac.jp>
2023-09-10 17:57:47 +0900

    ゼミ＆カテゴリー作成画面の見た目

--- resources/views/posts/category_create.blade.php
+++ resources/views/posts/category_create.blade.php
@@ -2,20 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <title>ゼミ・サークル紹介</title>
     </head>
     <body>
         <x-app-layout>
     <x-slot name="header">
-        　　チーム開発
+        　　カテゴリー作成
         　　 </x-slot>
-        <h1>チーム開発会へようこそ！</h1>
-        <h2>サークル投稿作成</h2>
+        <h1 style="font-size:25px; margin:20px">ようこそ！</h1>
+        <h2>カテゴリー作成</h2>
         <form action="/category/create" method="POST" enctype="multipart/form-data" >
             @csrf
             <div>
                 <h2>カテゴリー名</h2>
-                <input type="text" name="category[name]" placeholder="活動名or研究分野" value="{{ old('post.title') }}"/>
+                <input type="text" name="category[name]" placeholder="活動名or研究分野" value="{{ old('post.title') }}" style='margin-bottom:20px'/>
                 <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             
@@ -27,14 +27,9 @@
                 
                 
             </select>
-            
-               
-            
                
                
-               
-               
-            <input type="submit" value="保存"/>
+            <p><input type="submit" value="保存" style='border:solid 1px;'/></p>
         </form>
         <div><a href="/">戻る</a></div>
         </x-app-layout>
